5a737e8285aaf3d77e36f14696b16725fd2e8738
Coolbuddy <akshkalyani@gmail.com>
2024-01-17 15:59:05 +0530

    added backend

--- application_to_take_DOB/app.js
+++ application_to_take_DOB/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3').verbose()
 
 const app = express();
 const PORT = 3000;
+
+const db = new sqlite3.Database("userdata.db");
+db.run(
+    "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, dob DATE)"
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,12 +18,23 @@ app.get('/', (req,res) => {
 
 });
 
-app.post('/submit', (req,res) => {
+app.post('/submit', (req, res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const dob = req.body.dob;
 
     res.send(`Submitted Information:<br>First Name: ${firstName}<br>Last Name: ${lastName}<br>Date of Birth: ${dob}`);
+
+    db.run("INSERT INTO users(name, surname, dob) VALUES(?,?,?)",
+    [firstName, lastName, dob],
+    function (err) {
+        if(err) {
+            return console.error(err.message);
+        }
+        console.log(`A new data has been added with id ${this.lastID}`);
+        console.log(`${firstName} || ${lastName} || ${dob}`)
+    })
+
 });
 
 //Start the server
