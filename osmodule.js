ff3b8afb55845bacce26cc9609976ab8a90c49e5
Coolbuddy <akshkalyani@gmail.com>
2024-01-17 10:56:36 +0530

    new learnings with Packages

---
+++ osmodule.js
@@ -0,0 +1,9 @@
+const os = require('node:os')
+
+// console.log(os.cpus())
+// console.log(os.arch())
+// console.log(os.endianness())
+// console.log(os.totalmem())
+// console.log(os.hostname())
+// console.log(os.networkInterfaces())
+// console.log(os.networkInterfaces())
