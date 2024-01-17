c4a3c8dd8a748e0b0783e6afff7964509b2948aa
Coolbuddy <akshkalyani@gmail.com>
2024-01-17 11:17:14 +0530

    as

--- second.js
+++ second.js
@@ -1,7 +1,13 @@
+// Importing Packages
 const os = require('os')
 
-for(var i = 1; i <= 5; i++){
-    console.log("\n\nCycle Number", i , "***************\n\n");
-    console.log("CPU Information:", os.cpus());
-    console.log("Memory status:", os.freemem());
+const user = os.userInfo()
+console.log(user.username)
+if(user.username == "Admin"){
+    for(var i = 1; i <= 5; i++){
+        console.log("\n\nCycle Number", i , "***************\n\n");
+        console.log("CPU Information:", os.cpus());
+        console.log("Memory status:", os.freemem());
+    }
 }
+
