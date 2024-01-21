59d90be7968143c0ce3e021af5c97dd7dbb52a89
Coolbuddy <akshkalyani@gmail.com>
2024-01-21 20:50:29 +0530

    Working db

--- application_to_take_DOB/app.js
+++ application_to_take_DOB/app.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const sqlite3 = require('sqlite3').verbose()
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", __dirname);
 
 const db = new sqlite3.Database("userdata.db");
 db.run(
@@ -13,19 +16,8 @@ db.run(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/public/index.html');
-    
-}); 
-
-app.get('/showdata', (req, res) => {
-    db.run("SELECT * FROM users"),
-    function (err) {
-        if(err) {
-            return console.error(err.message);
-        }
-        console.log('The data base contains the following information');
-    }
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/submit', (req, res) => {
@@ -33,25 +25,29 @@ app.post('/submit', (req, res) => {
     const lastName = req.body.lastName;
     const dob = req.body.dob;
 
-    res.send(`Submitted Information:<br>First Name: ${firstName}<br>Last Name: ${lastName}<br>Date of Birth: ${dob}`);
+    // res.send(`Submitted Information:<br>First Name: ${firstName}<br>Last Name: ${lastName}<br>Date of Birth: ${dob}`);
 
     db.run("INSERT INTO users(name, surname, dob) VALUES(?,?,?)",
-    [firstName, lastName, dob],
-    function (err) {
-        if(err) {
-            return console.error(err.message);
+        [firstName, lastName, dob],
+        function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`A new data has been added with id ${this.lastID}`);
+            console.log(`${firstName} || ${lastName} || ${dob}`);
         }
-        console.log(`A new data has been added with id ${this.lastID}`);
-        console.log(`${firstName} || ${lastName} || ${dob}`)
-    })
+    );
 
+    db.all("SELECT * FROM users", (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send("Internal Server Error");
+        } else {
+            res.render("admin", { data: rows });
+        }
+    });
 });
 
-
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// 
